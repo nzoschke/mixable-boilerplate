842d019a08e25c376cf2b0400120d71f55d9b227
Noah Zoschke <noah@heroku.com>
2013-06-20 19:57:16 -0700

    temp playlist is working!

--- scripts/mixable.js
+++ scripts/mixable.js
@@ -7,6 +7,9 @@ require([
   var doPlaylistForAlbum = function() {
     var album = models.Album.fromURI('spotify:album:5rCCCernTo6IwFwEZM4H53');
     var list = List.forAlbum(album);
+
+    var playlist = models.Playlist.fromURI('spotify:user:nzoschke:playlist:1WZbSSUhher76SwrTEWvQx');
+    var list = List.forPlaylist(playlist);
     document.getElementById('playlistContainer').appendChild(list.node);
     list.init();
   };
@@ -16,19 +19,72 @@ require([
     var url = "http://developer.echonest.com/api/v4/song/search?api_key=FILDTEOIK2HBORODV"
 
     var onSearchSuccess = function(data) {
-      console.log(data);
+      models.Playlist.createTemporary().done(function(playlist) {
+        playlist.load("tracks").done(function() {
+          console.log(playlist)
 
-      var playlist = new models.Playlist();
-      for (var i=0; i<data.response.songs.length; i++) {
-        console.log(data.response.songs[i]);
-      }
+          for (var i=0; i < data.response.songs.length; i++) {
+            var song = data.response.songs[i];
+            console.log(song);
+
+            if (song.tracks.length > 0) {
+              var track_uri = song.tracks[0].foreign_id.replace("-WW", "");
+              var track = models.Track.fromURI(track_uri);
+            } else {
+              var track = models.Track.fromURI("spotify:local:Metro+Area:mixable:Nerves:387");
+            }
+
+            playlist.tracks.add(track);
+          }
+
+          var list = List.forPlaylist(playlist);
+          document.getElementById("playlistContainer").appendChild(list.node);
+          list.init();
+        });
+        //document.getElementById('playlist').innerHTML = playlist.name.decodeForHtml();
+      });
+
+      // var playlist = models.Playlist.fromURI('spotify:user:nzoschke:playlist:1WZbSSUhher76SwrTEWvQx');
+      // playlist.load("tracks").done(function() {
+      //   var collection = playlist.tracks;
+
+      //   for (var i=0; i < data.response.songs.length; i++) {
+      //     var song = data.response.songs[i];
+      //     console.log(song);
+
+      //     if (song.tracks.length > 0) {
+      //       var track_uri = song.tracks[0].foreign_id.replace('-WW', '');
+      //       var track = models.Track.fromURI(track_uri);
+      //       collection.add(track);
+      //     }
+      //   }
+
+      //   var list = List.forCollection(collection);
+      //   document.getElementById("playlistContainer").appendChild(list.node);
+      //   list.init();
+      // });
+
+      //var collection = new models.Collection();
+      //console.log(collection);
+      //console.log(data);
+
+
+
+      // //var list = List.forCollection(collection)
+      // //document.getElementById('playlistContainer').appendChild(list.node);
+      // //list.init();
+
+      // var album = models.Album.fromURI('spotify:album:5rCCCernTo6IwFwEZM4H53');
+      // var list = List.forAlbum(album);
+      // document.getElementById('playlistContainer').appendChild(list.node);
+      // list.init();
     };
 
     $.getJSON(url, {
       bucket: ["audio_summary", "id:spotify-WW", "tracks"], // id:musicbrainz => foreign_release_id: "musicbrainz:release:0299c4da-dd58-4d4f-be3d-388a0ffc9e31"
       combined: "radiohead",
       format: "json",
-      results: 50,
+      results: 2,
       sort: "song_hotttnesss-desc"
     }, onSearchSuccess);
   }
