c0ead6b0bce3dff24fd398a7eb6726288dcaf041
Noah Zoschke <nzoschke@gmail.com>
2013-12-23 16:18:47 -0800

    finalize import script

--- spotify/import.sh
+++ spotify/import.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-rm -rf tmp && mkdir tmp && cd tmp
+rm -rf scripts tmp
 
-cp /Applications/Spotify.app/Contents/Resources/apps.zip .
-unzip apps.zip api.1 views.1
+mkdir tmp
+(
+  cd tmp
+  cp /Applications/Spotify.app/Contents/Resources/apps.zip .
 
-unzip api.1   scripts/* && mv scripts ../api
-unzip views.1 scripts/* && mv scripts ../views
-
-find ../views ../api -name '*.js' | xargs -I% -L1 -t js-beautify/js-beautify -s2 -o % %
+  for m in api views; do
+    unzip apps.zip $m.1
+    unzip $m.1 scripts/*
+    (
+      cd scripts
+      find . -name '*.js' | xargs -I% -L1 -t ../../js-beautify/js-beautify -o ../../scripts/$m/% -s2 %
+    )
+    rm -rf scripts
+  done
+)
+rm -rf tmp
